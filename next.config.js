3ffcf94430e8f1ccc8eb9f29aa99c27c5cbe03d8
Sumon <sumoncpi10@gmail.com>
2023-10-11 01:06:49 +0600

    allokkk

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ const nextConfig = {
 
 module.exports = {
   images: {
-    domains: ['joeschmoe.io','lh3.googleusercontent.com','avatars.githubusercontent.com','www.startech.com.bd','pbsofficeinfosql.onrender.com','pbsofficeinfomongodb.onrender.com','localhost:5000','file-chittagong.portal.gov.bd','pbsactivities-server.vercel.app'],
+    domains: ['https://computer-management-backend-iiscez3bd-nmshohel.vercel.app', 'joeschmoe.io', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'www.startech.com.bd', 'pbsofficeinfosql.onrender.com', 'pbsofficeinfomongodb.onrender.com', 'localhost:5000', 'file-chittagong.portal.gov.bd', 'pbsactivities-server.vercel.app'],
   },
 };
